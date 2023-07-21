21b99e90df3efb6654fb428840b008a4edca778f
ahmad <heydarpanah92>
2023-07-21 20:27:49 +0330

    add prev and next shortcuts to harpoon

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -11,4 +11,6 @@ vim.keymap.set("n", "<A-1>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<A-2>", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<A-3>", function() ui.nav_file(3) end)
 vim.keymap.set("n", "<A-4>", function() ui.nav_file(4) end)
-vim.keymap.set("n", "<C-`>", function() tmux.gotoTerminal("{down-of}") end)
+
+vim.keymap.set("n", "<A-o>", function() ui.nav_prev() end)
+vim.keymap.set("n", "<A-i>", function() ui.nav_next() end)
